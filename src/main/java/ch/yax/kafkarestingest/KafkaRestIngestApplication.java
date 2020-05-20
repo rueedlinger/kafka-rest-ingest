7940d2c79e3e090715c5990e8784cf062bbfdc79
Matthias Rüedlinger <matthias.ruedlinger@spoud.io>
2020-05-20 21:32:43 +0200

    initial commit

---
+++ src/main/java/ch/yax/kafkarestingest/KafkaRestIngestApplication.java
@@ -0,0 +1,13 @@
+package ch.yax.kafkarestingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRestIngestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRestIngestApplication.class, args);
+	}
+
+}
